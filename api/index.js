e9397371b7758f2c937c0b181c73dace02ef6f34
Heracc <73125351+Heracc@users.noreply.github.com>
2025-01-09 09:22:04 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,3 @@ export default function handler(req, res) {
   res.writeHead(302, { Location: '/api/get-ip' });
   res.end();
 }
-export default function handler(req, res) {
-  res.writeHead(302, { Location: '/api/get-ip' });
-  res.end();
-}
