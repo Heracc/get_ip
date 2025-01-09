a19a93edea03ff9f70ac62aa7772937b121e629b
Heracc <73125351+Heracc@users.noreply.github.com>
2025-01-09 09:19:21 -0800

    Update get-ip.js

--- api/get-ip.js
+++ api/get-ip.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log('Visitor IP:', ip);
-  res.status(200).send('IP logged');
+  res.status(200).send('hehe');
 }
