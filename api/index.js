beaab840dc313c2f20d9290842c3fdebce667dad
Heracc <73125351+Heracc@users.noreply.github.com>
2025-01-09 09:18:38 -0800

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.writeHead(302, { Location: '/api/get-ip' });
+  res.end();
+}
+export default function handler(req, res) {
+  res.writeHead(302, { Location: '/api/get-ip' });
+  res.end();
+}
